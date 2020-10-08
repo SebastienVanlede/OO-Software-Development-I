d8b0390af17be1028822055fff7655ae699458dc
SebastienVanlede <56797089+SebastienVanlede@users.noreply.github.com>
2020-10-08 16:19:20 +0200

    .

--- Hoofdstuk 1/Voorbeeldoefeningen Hoofdstuk 1/src/Oefening1.java
+++
@@ -1,9 +0,0 @@
-
-public class Oefening1 {
-
-	public static void main(String[] args) {
-		System.out.printf("%s%n%s%n%s%n%s%n%s%n", "Open de map C:\\Mijn Documenten", "Volg de pijl\"Omleiding\"",
-				"Het boek voor \t Java", "is dat van \t\t\t Deitel.", "Engelstalig");
-	}
-
-}
