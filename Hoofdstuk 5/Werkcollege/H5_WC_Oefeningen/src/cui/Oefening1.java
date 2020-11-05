0c9e2127e371d0ddb2a04d9f4a17a520f174a2c2
SebastienVanlede <56797089+SebastienVanlede@users.noreply.github.com>
2020-11-05 16:37:47 +0100

    Toevoegen oef WC H5
    Oef 1 + Oef 4

---
+++ Hoofdstuk 5/Werkcollege/H5_WC_Oefeningen/src/cui/Oefening1.java
@@ -0,0 +1,43 @@
+package cui;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Oefening1 {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		// invoer double stop -1
+		Scanner input = new Scanner(System.in);
+		double bedrag;
+		List<Double> deLijst = new ArrayList<>();
+
+		System.out.print("Geef positief bedrag, stop met -1: ");
+		bedrag = input.nextDouble();
+
+		while (bedrag != -1) {
+			if (bedrag >= 0) {
+				deLijst.add(bedrag);
+			}
+
+			System.out.print("Geef positief bedrag, stop met -1: ");
+			bedrag = input.nextDouble();
+		}
+		if (deLijst.isEmpty()) {
+			System.out.println("De lijst is leeg!");
+		} else {
+			System.out.print("De lijst met bedragen: ");
+			for (int index = 0; index < deLijst.size(); index++) {
+				System.out.printf("%8.2f", deLijst.get(index));
+			} // mogelijke oplossing
+
+			System.out.print("De lijst met bedragen: ");
+			for (double element : deLijst) {
+				System.out.printf("%8.2f", element);
+			} // mogelijke oplossing
+		}
+
+	}
+
+}
---
+++ Hoofdstuk 5/Werkcollege/H5_WC_Oefeningen/src/cui/Oefening4.java
@@ -0,0 +1,32 @@
+package cui;
+
+import java.util.Scanner;
+
+public class Oefening4 {
+
+	@SuppressWarnings({ "resource", "unused" })
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double[] maanden = new double[12];
+		double totaal = 0, gemiddelde;
+		String resultaat = "";
+
+		for (int index = 0; index < maanden.length; index++) {
+			System.out.printf("Geef de uitgaven in voor maand %d: ", index + 1);
+			maanden[index] = input.nextDouble();
+			totaal += maanden[index];
+		}
+		gemiddelde = totaal / maanden.length;
+		for (int index = 0; index < maanden.length; index++) {
+			if (maanden[index] > gemiddelde) {
+				resultaat += String.format("%d ", index + 1);
+			}
+		}
+		if (resultaat.isEmpty()) { // .length / == 0 / .equals("")
+			System.out.println("Er zijn geen maanden waarin meer werd uitgegeven dan het gemiddelde.");
+		} else {
+			System.out.printf("In volgende maanden werd meer uitgegeven dan het gemiddelde %s: ", resultaat);
+		}
+	}
+
+}
