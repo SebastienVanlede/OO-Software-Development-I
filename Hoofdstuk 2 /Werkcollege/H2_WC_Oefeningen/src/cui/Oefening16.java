9ea9ca03ec20d5537dccca9129ccc76a84039659
SebastienVanlede <56797089+SebastienVanlede@users.noreply.github.com>
2020-11-05 14:36:39 +0100

    name fixes

---
+++ Hoofdstuk 2 /Werkcollege/H2_WC_Oefeningen/src/cui/Oefening16.java
@@ -0,0 +1,38 @@
+package cui;
+
+public class Oefening16 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Patroon A:");
+		for (int rij = 1; rij <= 10; rij++) { // zorgt voor 10 rijen
+			for (int kol = 1; kol <= rij; kol++) {
+				System.out.print('*');
+			}
+			System.out.println();
+
+		}
+
+		System.out.println("Patroon C:");
+		for (int rij = 1; rij <= 10; rij++) {
+			for (int kol = 10; kol >= rij; kol--) {
+				System.out.print('*');
+			}
+			System.out.println();
+
+		}
+
+		System.out.println("Patroon B");
+		for (int rij = 1; rij <= 10; rij++) {
+			for (int spatie = 1; spatie <= rij - 1; spatie++) {
+				System.out.print(' ');
+			}
+			for (int ster = 10; ster >= rij; ster--) {
+				System.out.print('*');
+			}
+			System.out.println();
+
+		}
+	}
+
+}
