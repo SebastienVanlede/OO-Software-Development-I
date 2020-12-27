694f58bf5b91faa352f8f8bb2b6f6d54742ee9df
Clusternite <48180370+Clusternite@users.noreply.github.com>
2020-12-27 17:23:08 +0100

    Update Oefening7_Behangen.java
    - added my way of solving the exercise in comment

--- Hoofdstuk 1 /Oefeningen/Oefeningen_H1/src/Oefeningen/Oefening7_Behangen.java
+++ Hoofdstuk 1 /Oefeningen/Oefeningen_H1/src/Oefeningen/Oefening7_Behangen.java
@@ -19,9 +19,26 @@ public class Oefening7_Behangen {
 		
 		System.out.printf("Het aantal benodigde rollen = %d%n", aantalRollen);
 		
-		
-		
-		
+		/*
+		// Other way (uncommenting will cause interference):
+		Scanner scanner = new Scanner(System.in);
+        	int hoogte, breedte;
+
+        	do {
+            	     System.out.printf("Geef de hoogte van de muur in cm: ");
+           	     hoogte = scanner.nextInt();
+       		} while (hoogte < 0 || hoogte > 1001);
+        
+         	do {
+           	     System.out.printf("Geef de breedte van de muur in cm: ");
+            	     breedte = scanner.nextInt();
+        	} while (breedte < 0 || breedte > 1001);
+
+        	double oppervlakte = breedte * hoogte;
+        	double nRollen = Math.ceil(oppervlakte / (1000 * 50));
+
+        	System.out.printf("Het aantal benodigde rollen = %d %n", (int)nRollen);
+		*/
 	}
 
 }
