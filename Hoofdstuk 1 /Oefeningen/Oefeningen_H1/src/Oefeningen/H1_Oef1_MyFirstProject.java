e4915ae27e2341cd0a7e04bf3458499e7fab6e46
Clusternite <48180370+Clusternite@users.noreply.github.com>
2020-12-27 16:57:01 +0100

    Add files via upload

---
+++ Hoofdstuk 1 /Oefeningen/Oefeningen_H1/src/Oefeningen/H1_Oef1_MyFirstProject.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class H1_Oef1_MyFirstProject {
+
+    public static void main(String[] args) {
+        int x = 3, y = 5, som;
+        som = x * x + y * y;
+        System.out.println("Som = " + som);
+    }
+
+}
