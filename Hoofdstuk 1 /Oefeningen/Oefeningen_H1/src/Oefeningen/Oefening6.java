2f4b96c48dcaf8f7201d6fd6365cc1354acd3eb9
Clusternite <48180370+Clusternite@users.noreply.github.com>
2020-12-27 17:14:29 +0100

    Update Oefening6.java
    - added my own version of the exercise in comment

--- Hoofdstuk 1 /Oefeningen/Oefeningen_H1/src/Oefeningen/Oefening6.java
+++ Hoofdstuk 1 /Oefeningen/Oefeningen_H1/src/Oefeningen/Oefening6.java
@@ -21,6 +21,30 @@ public class Oefening6 {
 
 		System.out.printf("%s%n%d%s%n%d%s%n%d%s%n%d%s%n", "Het getal bestaat uit:", duizendtallen, " duizendtallen",
 				honderdtallen, " hondertallen", tientallen, " tientallen", eenheden, " eenheden");
+	/*
+	// Other:
+	Scanner scanner = new Scanner(System.in);
+        int inputNumber;
+        
+	do {
+            System.out.printf("Give me a number from 999 to 9999: ");
+            inputNumber = scanner.nextInt();
+        } while (inputNumber <= 1000 || inputNumber > 9999);
+
+        int duizendtallen, honderdtallen, tientallen, eenheden;
+	
+        duizendtallen = inputNumber / 1000;
+        honderdtallen = (inputNumber - duizendtallen * 1000) / 100;
+        tientallen = (inputNumber - duizendtallen * 1000 - honderdtallen * 100) / 10;
+        eenheden = (inputNumber - duizendtallen * 1000 - honderdtallen * 100 - tientallen * 10);
+
+        System.out.printf("%d duizendtallen %n" +
+                        "%d honderdtallen %n" +
+                        "%d tientallen %n" +
+                        "%d eenheden %n",
+                duizendtallen, honderdtallen, tientallen, eenheden);
+	*/
+	
 	}
 
 }
