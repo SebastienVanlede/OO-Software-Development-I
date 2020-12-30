16264f4249afe5443c388fd2c44c5e533a0641f4
Clusternite <48180370+Clusternite@users.noreply.github.com>
2020-12-30 10:36:32 +0100

    Update PatroonApplicatie.java
    - added my version of the exercise in comment

--- Hoofdstuk 3/Oefeningen/H3_Oefeningen/src/cui/PatroonApplicatie.java
+++ Hoofdstuk 3/Oefeningen/H3_Oefeningen/src/cui/PatroonApplicatie.java
@@ -8,6 +8,16 @@ public class PatroonApplicatie {
 		Patroon patroon = new Patroon('D', '*');
 		
 		System.out.print(patroon.teken());
+		
+		/*
+		// Other way (uncommenting can cause interference):
+		Patroon patroon;
+
+		for (char alphabet = 'A'; alphabet <= 'D'; alphabet++) {
+		    patroon = new Patroon(alphabet, '*');
+		    System.out.printf(patroon.teken());
+		}
+		*/
 	}
 
 }
