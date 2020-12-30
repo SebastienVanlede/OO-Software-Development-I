86465c712c40fd75ec6a0f11e7e3d4347ad90603
Clusternite <48180370+Clusternite@users.noreply.github.com>
2020-12-30 10:49:17 +0100

    Update Auto.java
    - added my version of the exercise in comment

--- Hoofdstuk 3/Oefeningen/H3_Oefeningen/src/domein/Auto.java
+++ Hoofdstuk 3/Oefeningen/H3_Oefeningen/src/domein/Auto.java
@@ -60,4 +60,62 @@ public class Auto {
 		
 	}
 	
+	/*
+	// Other way (uncommenting can cause interference):
+	// ------------------- attribute -------------------
+	    private String kleur, merk, nrplaat;
+	    private int snelheid;
+
+	    // ------------------- Constructor -------------------
+	    public Auto() {
+		this("grijs");
+	    }
+
+	    public Auto(String kleur) {
+		this(kleur, "");
+	    }
+
+	    public Auto(String kleur, String merk) {
+		this.setKleur(kleur);
+		this.setMerk(merk);
+		this.setNrplaat("");
+		this.snelheid = 0;
+	    }
+
+	    public void versnel(int extraSnelheidErbij) {
+		if (this.snelheid > 220) {
+		    this.snelheid = 220;
+		}
+	    }
+
+
+	    // ------------------- getters and setters -------------------
+
+	    public String getKleur() {
+		return kleur;
+	    }
+
+	    public void setKleur(String kleur) {
+		this.kleur = kleur;
+	    }
+
+	    public String getMerk() {
+		return merk;
+	    }
+
+	    private void setMerk(String merk) {
+	    }
+
+	    public String getNrplaat() {
+		return nrplaat;
+	    }
+
+	    public void setNrplaat(String nrplaat) {
+		this.nrplaat = nrplaat;
+	    }
+
+	    public int getSnelheid() {
+		return snelheid;
+	    }
+	*/
 }
