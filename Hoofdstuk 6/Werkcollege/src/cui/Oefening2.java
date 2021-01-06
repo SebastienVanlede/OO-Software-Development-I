ad214159c09bf4c46a066ac445ecf9632ef249ea
Clusternite <48180370+Clusternite@users.noreply.github.com>
2021-01-06 14:04:14 +0100

    Update Oefening2.java
    whitespace and shortened to scanner

--- Hoofdstuk 6/Werkcollege/src/cui/Oefening2.java
+++ Hoofdstuk 6/Werkcollege/src/cui/Oefening2.java
@@ -5,18 +5,20 @@ import java.util.Scanner;
 public class Oefening2 {
 
 	public static void main(String[] args) {
-		Scanner inputScanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		String schaal = "Celcius";
+		
 		System.out.printf("Geef de temperatuur in graden %s (9999 om te stoppen): ", schaal);
-		int temp = inputScanner.nextInt();
+		int temp = scanner.nextInt();
+		
 		while (temp != 9999) {
 			String resultaat = geefTemperatuurStatus(temp);
 			System.out.printf("%d graden %s voelt aan als %s%n", temp, schaal, resultaat);
 			System.out.printf("Geef de temperatuur in graden %s (9999 om te stoppen): ", schaal);
-			temp = inputScanner.nextInt();
+			temp = scanner.nextInt();
 		}
 
-		inputScanner.close();
+		scanner.close();
 	}
 
 	private static String geefTemperatuurStatus(int temp) {
